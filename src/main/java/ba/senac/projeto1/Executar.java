fb74bb4fb094d56be8693d418e94b98060d61362
Ricardo Carvalho <rickmc123@gmail.com>
2021-11-18 21:27:34 -0300

    Mudei o algoritmo bolha para ola mundo

--- src/main/java/ba/senac/projeto1/Executar.java
+++ src/main/java/ba/senac/projeto1/Executar.java
@@ -11,22 +11,8 @@ public class Executar {
     
     public static void main (String args[]){
         
-        //Scanner ler = new Scanner(System.in);
-        int lista[]= {1,3,2,7,0,43,17,-10,-55};
-        int x;
-
-          for(int i = 0; i < lista.length; i++){
-              for(int j = 0; j < lista.length; j++){
-                  if(lista[i] < lista[j]){
-                      x = lista[i];
-                      lista[i] = lista[j];
-                      lista[j] = x;
-                  }
-              } 
-          }
-          for(int r = 0; r < lista.length; r++){
-              System.out.println(lista[r]);
-          }        
+        System.out.println("Hello World!");
+          
     }
     
 }
